9138e4794022c43736a9d202a4604284819ecdbc
BJR-Developer <freelancerbjr@gmail.com>
2022-01-03 22:52:59 +0600

    check

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-// const MONGODB_URL = 'mongodb+srv://bjrweatherforecast:BjrWeatherForecast@weather.jl5yz.mongodb.net/chat?retryWrites=true&w=majority'
-mongoose.connect('mongodb://localhost:27017/freelancerbjr' || process.env.MONGODB_URL , {useNewUrlParser: true  } ).then(connect=>{
+mongoose.connect('mongodb+srv://bjrweatherforecast:BjrWeatherForecast@weather.jl5yz.mongodb.net/chat?retryWrites=true&w=majority', {useNewUrlParser: true  } ).then(connect=>{
     console.log("MongoDB Connected");
 }).catch(err=>{
     console.log(err);
--- src/middleware/token.js
+++ src/middleware/token.js
@@ -3,7 +3,7 @@ dotenv.config();
 const jwt = require('jsonwebtoken')
 
 function genToken(email) {
-    return jwt.sign({email} , process.env.TOKEN_SECRET , {expiresIn: '1h'})
+    return jwt.sign({email} , 'f672618ad818a21e961d46ddac3033cd0e9a6eb508375dcc5ee5eb53c101885354e7dacdf492176fd697ab1b5992dccb0d0afb658a9db3bc65c068e91e997884' , {expiresIn: '1h'})
 }
 
 function authToken(req,res,next) {
