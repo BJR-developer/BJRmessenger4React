01a522ac25e8c7fce2ea94b12bc95b279cb35d6a
BJR-Developer <freelancerbjr@gmail.com>
2022-01-11 09:02:18 +0600

    database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require('mongoose')
 const localMongo = 'mongodb://localhost:27017/freelancerbjr'
-mongoose.connect( localMongo || process.env.MONGODB_URI , {useNewUrlParser: true  } ).then(connect=>{
+mongoose.connect(  process.env.MONGODB_URI , {useNewUrlParser: true  } ).then(connect=>{
     console.log("MongoDB Connected");
 }).catch(err=>{
     console.log(err);
